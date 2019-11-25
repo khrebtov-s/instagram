cdb72a6a13c1550b9ae950730e4a3f13e70560ca
Sergey Khrebtov <sergey.khrebtov@sibers.com>
2019-11-25 16:38:00 +0700

    changed home view component

--- src/components/presentation/home-view.js
+++ src/components/presentation/home-view.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import User from './user-view';
-import Posts from '../container/user-posts';
+import Posts from '../container/posts';
 // import Footer from './FooterView';
 
 const Home = props => {
