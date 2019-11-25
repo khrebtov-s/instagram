149e706a5e9d72d138e3c84de6c5559ed5fbb335
Sergey Khrebtov <sergey.khrebtov@sibers.com>
2019-11-25 16:36:28 +0700

    added proveder, and save state in localhost

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// special helper to pass the store as prop to all the components
+import { Provider } from 'react-redux';
+
+// redux persist stuffs
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+// create a redux store
+import { createStore } from 'redux';
+import appReducer from './reducers';
+
+import registerServiceWorker from './registerServiceWorker';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, appReducer);
+
+//crate store
+let store = createStore(persistedReducer);
+
+let persistor = persistStore(store);
 
 
+ReactDOM.render(
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
+    ,
+    document.getElementById('root'));
+
+registerServiceWorker();
